7b14b1a9d3e07edd320fe24a6afde302e715a115
tarcizio502 <tarciziogoncalves502@mail.com>
2025-02-09 14:58:58 +0000

    Meu primeiro projeto, feito por me! casa dos pets.

---
+++ script.js
@@ -0,0 +1,23 @@
+   
+    let count = 1; 
+    document.getElementById("radio1") .checked = true;  
+
+    setInterval( function(){  
+        nextImage();
+    }, 1000)    
+
+    function nextImage(){   
+        count++;  
+        if(count>24){  
+            count = 1;
+        }  
+
+        document.getElementById("radio"+count).checked = true; 
+
+    }
+
+
+    
+  
+  
+      
